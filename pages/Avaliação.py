51bd7ce3d30252bdda34c4b0a388ebf426ff1d97
Victor Viola <victor.viola@hvarconsulting.com.br>
2023-09-08 13:33:37 +0200

    .

--- pages/Avaliação.py
+++ pages/Avaliação.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
+counter = 0
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
-        if message["role"] == "assistant": 
+        if message["role"] == "assistant":
+            counter = counter + 1
             st.write("Essa resposta está correta?")
-            st.button("Sim")
-            st.button("Não")
+            st.button("Sim", key=str(counter) + "_yes")
+            st.button("Não", key=str(counter) + "_no")
