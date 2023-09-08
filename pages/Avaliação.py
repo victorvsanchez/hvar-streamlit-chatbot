9073ac1d21ab73936c4981bcd4cc3d55f14d2026
Victor Viola <victor.viola@hvarconsulting.com.br>
2023-09-08 13:30:49 +0200

    Added buttons

--- pages/Avaliação.py
+++ pages/Avaliação.py
@@ -4,4 +4,6 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
         if message["role"] == "assistant": 
-            st.write("Você gostou dessa resposta? - SIM / NÃO")
+            st.write("Essa resposta está correta?")
+            st.button("Sim")
+            st.button("Não")
