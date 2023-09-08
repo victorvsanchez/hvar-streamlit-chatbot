a84358c3ff2868e1ea54ef1d310498dccbbe2e02
Victor Viola <victor.viola@hvarconsulting.com.br>
2023-09-08 11:51:41 +0200

    Testing evaluation page

--- pages/Avaliação.py
+++ pages/Avaliação.py
@@ -3,5 +3,5 @@ import streamlit as st
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
-        if message["role"] == "assistant":
-            print("Você gostou dessa resposta?")
+        if message["role"] == "assistant": 
+            st.write("Você gostou dessa resposta? - SIM / NÃO")
