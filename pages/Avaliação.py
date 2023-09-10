d1c1b10ccb073222da65d9f60aa7329d195d038c
Victor Viola <victor.viola@hvarconsulting.com.br>
2023-09-10 12:11:45 +0200

    Minor change to Avaliação

--- pages/Avaliação.py
+++ pages/Avaliação.py
@@ -6,7 +6,7 @@ data = pd.read_csv("data/former_questions.csv", sep=";")
 for i in data.index:
     with st.chat_message(data["role"][i]):
         st.write(data["message"][i])
-        if not data["graded"][i]:
+        if not data["graded"][i] and data["role"][i]=="assistant":
             st.write("Essa resposta está correta?")
             st.button("Sim", key=str(i) + "_yes")
             st.button("Não", key=str(i) + "_no")
