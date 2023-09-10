1d29910b2f20b228f6b93a1510109260663ca61c
Victor Viola <victor.viola@hvarconsulting.com.br>
2023-09-10 15:16:30 +0200

    Minor logic fix

--- pages/Avaliação.py
+++ pages/Avaliação.py
@@ -6,9 +6,10 @@ data = pd.read_csv("data/former_questions.csv", sep=";")
 #Add new data
 msg_num = 0
 for message in st.session_state.messages:
-    msg_num = msg_num + 1
     new_record = ["Admin", msg_num, message["role"], message["content"], 0, 1]
-    data.loc[len(data.index)] = new_record
+    if msg_num >= 1:
+        data.loc[len(data.index)] = new_record
+    msg_num = msg_num + 1
 
 for i in data.index:
     with st.chat_message(data["role"][i]):
